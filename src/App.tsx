cbf42ccb0e463feacd32fbe3964808bddcbc809f
mathpss <matheus.p.souza.silva@gmail.com>
2026-01-22 07:38:48 -0300

    build:criado a base do projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        Landing page
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest/config" />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/tests/setup.ts',
+  },
+})
